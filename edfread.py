b05f2c2ada766e80e99cb1ed4b3acb71d427fa36
liujiamei <jiamei_liu@yeah.net>
2018-06-01 15:19:02 +0800

    3s slide window

--- edfread.py
+++ edfread.py
@@ -44,7 +44,6 @@ def read_edf(file_path, start_time=None, end_time=None):
 #对原始的edf文件信道进行处理，去掉多余的信道
 def channel_handle(file_path):
     basename = os.path.basename(file_path)
-    print basename
     head = basename.split('_')[0]
     normal_file = [
                 'chb01', 'chb02', 'chb03', 'chb04', 
@@ -56,11 +55,11 @@ def channel_handle(file_path):
             'chb21', 'chb22']
 
     if head in normal_file:
-        return list(xrange(22))
+        return list(range(0, 22))
     elif head in modify_file:   
         # handle special case of chb11_01
         if basename == 'chb11_01.edf':
-            return list(xrange(22))
+            return list(range(0, 22))
         else:
             return [0,1,2,3,5,6,7,8,13,14,15,16,18,19,20,21,10,11,23,24,25,26]
     elif 'chb15' in head:
@@ -99,9 +98,7 @@ def read_summary(summary_path):
             i += 1
     return file_info
 
-
-
-def edf_read_from_folder(edf_dir, save_dir):
+def read_raw_data(edf_dir):
     edf_file_list = []
     summary_file_path = ''
     # get all edf file path in folder
@@ -126,10 +123,12 @@ def edf_read_from_folder(edf_dir, save_dir):
     seizure_total_time = 0
     for file_path in edf_file_list:
         basename = os.path.basename(file_path)
-        info = (item for item in file_info if item["filename"] == basename).next()
-        if info is None:
+        info = [item for item in file_info if item["filename"] == basename]
+        if info is None or len(info) == 0:
             info = {}
             info['num_seizure'] = 0
+        else:
+            info = info[0]
         if info['num_seizure'] > 0:
             # extrach seizure time in file
             print('reading seizure data from {}'.format(file_path))
@@ -153,6 +152,37 @@ def edf_read_from_folder(edf_dir, save_dir):
     print('seizure raw data shape {}'.format(np.shape(seizure_raw_data)))
     # extract enough none-seizure data according to seizure total time
     print('total seizure time: {}'.format(seizure_total_time))
+    return seizure_raw_data, no_seizure_raw_data
+
+
+def edf_read_3s_slide_window(edf_dir, save_dir):
+
+    dir_name = os.path.basename(edf_dir)
+    seizure_raw_data, no_seizure_raw_data = read_raw_data(edf_dir)
+    # slide 3s window on seizure_data
+    seizure_data = slide_data(seizure_raw_data, 3)
+    no_seizure_data = slide_data(no_seizure_raw_data, 3)
+    
+    print('seizure data shape: {}'.format(np.shape(seizure_data)))
+    print('no seizure data shape: {}'.format(np.shape(no_seizure_data)))
+    if np.shape(seizure_data)[0] < np.shape(no_seizure_data)[0]:
+        no_seizure_data = random.sample(no_seizure_data, np.shape(seizure_data)[0])
+
+    print('seizure data shape: {}'.format(np.shape(seizure_data)))
+    print('no seizure data shape: {}'.format(np.shape(no_seizure_data)))
+    label = [0]*np.shape(seizure_data)[0] + [1]*np.shape(seizure_data)[0]
+    # data format (sample, channel, feature) feature is 6s data with 256 SampFreq
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+    data = no_seizure_data + seizure_data
+    np.save(os.path.join(save_dir, '{}_data.npy'.format(dir_name)), data)
+    np.save(os.path.join(save_dir, '{}_label.npy'.format(dir_name)), label)
+    
+
+
+def edf_read_6s_slice(edf_dir, save_dir):
+    dir_name = os.path.basename(edf_dir)
+    seizure_raw_data, no_seizure_raw_data = read_raw_data(edf_dir)
     seizure_data = []
     no_seizure_data = []
     # modify seizure data, 6 sec as a sample
@@ -186,16 +216,39 @@ def slice_data(input_data, slice_size):
     for item in input_data:
         raw_size = np.shape(item)[1]
         slice_num = int(math.floor(raw_size/slice_size))
-        for i in xrange(0, slice_num):
+        for i in range(0, slice_num):
             out_data.append(slice_regard_channel(item, i*slice_size, (i+1)*slice_size))
                     
     print('out_size {}'.format(np.shape(out_data)))
     return out_data
 
+def slide_data(input_data, window_size):
+    # TODO code optimization!!
+
+    out_data = []
+    def slice_regard_channel(data, start, end):
+        out = []
+        for channel in data:
+            chunks = channel[start*SampFreq:end*SampFreq]
+            out.append(chunks)
+        return out
+
+    for item in input_data:
+        raw_size = np.shape(item)[1]
+        slide_window_num = (raw_size - window_size*SampFreq)/SampFreq
+        slide_window_num = int(math.floor(slide_window_num))
+        print('raw_size: {}, window_num: {}'.format(raw_size, slide_window_num))
+        for i in range(slide_window_num):
+            start = i
+            end = i + window_size
+            out_data.append(slice_regard_channel(item, start, end))
+
+                    
+    print('out_size {}'.format(np.shape(out_data)))
+    return out_data
 
 def test():
     data = np.load('seizure_raw_data.npy')
-    print np.shape(data[0][0])
     print('input size: {}'.format(np.shape(data)))
     x_data = slice_data(data, 6*SampFreq)
 
@@ -203,7 +256,7 @@ def main():
     args = get_parser()
     edfpath = args.folder
     save_dir = args.save_dir
-    edf_read_from_folder(edfpath, save_dir)
+    edf_read_3s_slide_window(edfpath, save_dir)
     
     
 if __name__ == '__main__':
